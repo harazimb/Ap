cff52d4701af96c5fb4de1956ca62627fb0cedad
Evan Moran <moraneva@cse.msu.edu>
2015-04-01 01:09:29 -0400

    Evan - Git works!!!!! Regristration

---
+++ app/src/main/java/com/harazim/android/ap/RegisterActivity.java
@@ -0,0 +1,218 @@
+package com.harazim.android.ap;
+
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class RegisterActivity extends Activity {
+
+    JSONParser jsonParser = new JSONParser();
+
+    // JSON Node names
+    private static final String TAG_SUCCESS = "success";
+
+    /**
+     * A dummy authentication store containing known user names and passwords.
+     * TODO: remove after connecting to a real authentication system.
+     */
+    private static final String[] DUMMY_CREDENTIALS = new String[]{
+            "foo@example.com:hello", "bar@example.com:world"
+    };
+    /**
+     * Keep track of the login task to ensure we can cancel it if requested.
+     */
+    private UserLoginTask mAuthTask = null;
+    private EditText mPasswordView;
+    private EditText mUsernameView;
+    private Button mRegisterButton;
+    private View mLoginFormView;
+
+    private static String url = "http://cse.msu.edu/~harazimb/register_app.php";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+
+        mPasswordView = (EditText) findViewById(R.id.password);
+        mUsernameView = (EditText) findViewById(R.id.username);
+
+        mRegisterButton= (Button) findViewById(R.id.register_button);
+        mRegisterButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptLogin();
+            }
+        });
+
+        mLoginFormView = findViewById(R.id.login_form);
+
+    }
+
+    /**
+     * Attempts to sign in or register the account specified by the login form.
+     * If there are form errors (invalid email, missing fields, etc.), the
+     * errors are presented and no actual login attempt is made.
+     */
+    public void attemptLogin()
+    {
+        //If there is already a authorization task going on, don't do anything.
+        if(mAuthTask!=null)
+        {
+            return;
+        }
+
+        //Reset errors.
+        mUsernameView.setError(null);
+        mPasswordView.setError(null);
+
+        // Get the values of both text fields
+        String username = mUsernameView.getText().toString();
+        String password = mPasswordView.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        //Checking for valid password
+        if(!TextUtils.isEmpty(password) && !isPasswordValid(password))
+        {
+            mPasswordView.setError(getString(R.string.error_invalid_password));
+            focusView = mPasswordView;
+            cancel = true;
+        }
+
+        //check for valid username
+        if(TextUtils.isEmpty(username))
+        {
+            mUsernameView.setError(getString(R.string.error_field_required));
+            focusView = mUsernameView;
+            cancel = true;
+        }
+        else if(!isUsernameValid(username))
+        {
+            mUsernameView.setError(getString(R.string.error_invalid_username));
+            focusView = mUsernameView;
+            cancel = true;
+        }
+
+        if(cancel)
+        {
+            //There was an error so focus on what field caused the error.
+            focusView.requestFocus();
+        }
+        else{
+            // Execute registration.
+            mAuthTask = new UserLoginTask(username,password);
+            mAuthTask.execute((Void) null);
+        }
+
+    }
+
+    /**
+     * Is the input password valid? Was thinking greater than 4 characters??
+     * @param password password entered
+     * @return true if valid
+     */
+    private boolean isPasswordValid(String password)
+    {
+        return password.length()>4;
+    }
+
+    /**
+     * Going to implement code to check if username already taken here.
+     * @param username username entered.
+     * @return true if valid.
+     */
+    private boolean isUsernameValid(String username)
+    {
+        return true;
+    }
+
+    /**
+     *
+     */
+    public class UserLoginTask extends AsyncTask<Void,Void,Boolean> {
+
+        private final String mUsername;
+        private final String mPassword;
+        UserLoginTask(String username, String password)
+        {
+            mUsername=username;
+            mPassword=password;
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... args)
+        {
+            //Create username/password param.
+            List<NameValuePair> params = new ArrayList<NameValuePair>();
+            params.add(new BasicNameValuePair("username",mUsername));
+            params.add(new BasicNameValuePair("password",mPassword));
+
+            // Getting json Object.
+            JSONObject json = jsonParser.makeHttpRequest(url,"POST",params);
+
+            try{
+                int success=json.getInt(TAG_SUCCESS);
+
+                if(success==1){
+
+                    finish();
+                } else{
+
+                }
+
+            }
+            catch(JSONException e){
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success)
+        {
+            mAuthTask=null;
+
+            if(success)
+            {
+                finish();
+            }
+            else
+            {
+                mPasswordView.setError(getString(R.string.error_incorrect_password));
+                mPasswordView.requestFocus();
+            }
+
+        }
+
+        @Override
+        protected void onCancelled()
+        {
+            mAuthTask=null;
+        }
+    }
+}
+
+
+
